3aa205e51414b84df2a7ec1d1e6e7f00e1f1c2ab
nochnoenebo <gomenasai989@gmail.com>
2020-03-30 15:45:15 +0300

    15 - 17

--- 15.py
+++ 15.py
@@ -1,15 +1,14 @@
 from sys import argv
 #берем 2 значения из командной строки
 script, filename = argv
-#переменная txt принимает содержимое Filename
-txt = open(filename)
+#переменная txt считывает содержимое Filename
+txt = open(filename, 'r')
 #вывод в строке названия файла
 print(f"Содержимое файла {filename}:")
 print(txt.read()) #вывод переменной txt
+print("Снова введите имя файла:")
+file_again = input("> ")
 
-#print("Снова введите имя файла:")
-#file_again = input("> ")
+txt_again = open(file_again)
 
-#txt_again = open(file_again)
-
-#print(txt_again.read())
+print(txt_again.read())
