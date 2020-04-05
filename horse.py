cdb26587ecd314408c02191a58ee26ca55468141
nochnoenebo <44203607+nochnoenebo@users.noreply.github.com>
2020-04-05 23:08:29 +0300

    не смотри пока, я наговнякала

--- horse.py
+++ horse.py
@@ -2,23 +2,28 @@ import random
 from random import randint
 i = randint(0, 7)
 j = randint(0, 7)
-print (i, j)
-m = 8
-n = 8
-matrix = [[0 for j in range(m)] for i in range(n)]
-if i in range(m):
-    for f in range(n):
-        matrix[i][j]= 1
-    print (matrix)
+print(i, j)
+
+matrix = [[0 for j in range(8)] for i in range(8)] #матрица
+matrix[i][j]=1
+print(matrix)
 
 
-i, j = (int(i) for i in input().split())
-#print (i, j)
-if 0<=i<=7 and 0<=j<=7:
-    matrix = [[0 for j in range(m)] for i in range(n)]
-    if i in range(m):
-        for f in range(n):
-            matrix[i][j]= 1
-        print (matrix)
+
+def coord():
+    (int(i) for i in input().split())
+x, y = coord()
+
+while x>7 or y>7:
+    print("что-то больше 7")
+    x, y = coord()
+if [x][y]==[i-1][j+2] or [x][y]==[i+1][j+2] or [x][y]==[i+2][j+1] or [x][y]==[i-2][j+1] or [x][y]==[i+2][j-1] or [x][y]==[i-2][j-1] or [x][y]==[i-1][j-2] or [x][y]==[i+1][j-2]:
+
+    matrix = [[0 for y in range(8)] for x in range(8)]
+    matrix[x][y] = 1
 else:
-    print ("fuck this shit.")
+    coord()
+
+#matrix = [[0 for j in range(m)] for i in range(m)]
+#matrix[i][j]=1
+print(matrix)
