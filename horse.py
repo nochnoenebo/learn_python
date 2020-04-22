7a7ec10f8999c177f8b45bcd5783c9e6c894e835
nochnoenebo <44203607+nochnoenebo@users.noreply.github.com>
2020-04-22 23:12:26 +0300

    Update horse.py

--- horse.py
+++ horse.py
@@ -9,18 +9,21 @@ matrix = [[0 for b in range(m)] for a in range(n)]
 matrix[a][b] = 1
 print(matrix)
 
-
 i, j = (int(i) for i in input().split())
+while i>7 or i<0 or j>7 or j<0:
+    i, j = (int(i) for i in input().split())
+
 # if 0 <= i <= 7 and 0 <= j <= 7:
-#     matrix = [[0 for j in range(m)] for i in range(n)]
-#     matrix[i][j]= 1
-#     print(matrix)
+#      matrix = [[0 for j in range(m)] for i in range(n)]
+#      matrix[i][j]= 1
+#      print(matrix)
 # else:
-#     print("возврат к вводу координат")
+#      print("возврат к вводу координат")
+
 if (abs(a - i) == 2 and abs(b - j) == 1) or (abs(a - i) == 1 and abs(b - j) == 2):
     print ("horse")
     matrix = [[0 for j in range(m)] for i in range(n)]
     matrix[i][j] = 1
     print(matrix)
 else:
-    print ("not_horse")
+    print ("надо опять возврат на ввод i j")
