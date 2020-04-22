ea0318ccf409c3d00b1858edba51215cf57e6587
nochnoenebo <44203607+nochnoenebo@users.noreply.github.com>
2020-04-22 22:14:53 +0300

    есть проверка на коня
    как сделать запрос ввода новых i j? сделать функцию?

--- horse.py
+++ horse.py
@@ -1,29 +1,32 @@
 import random
 from random import randint
-i = randint(0, 7)
-j = randint(0, 7)
-print(i, j)
+a = randint(0, 7)
+b = randint(0, 7)
+print(a, b)
+m = 8
+n = 8
+matrix = [[0 for b in range(m)] for a in range(n)]
+if a in range(m):
+    for f in range(n):
+        matrix[a][b] = 1
+    print(matrix)
 
-matrix = [[0 for j in range(8)] for i in range(8)] #матрица
-matrix[i][j]=1
-print(matrix)
 
-
-
-def coord():
-    (int(i) for i in input().split())
-x, y = coord()
-
-while x>7 or y>7:
-    print("что-то больше 7")
-    x, y = coord()
-if [x][y]==[i-1][j+2] or [x][y]==[i+1][j+2] or [x][y]==[i+2][j+1] or [x][y]==[i-2][j+1] or [x][y]==[i+2][j-1] or [x][y]==[i-2][j-1] or [x][y]==[i-1][j-2] or [x][y]==[i+1][j-2]:
-
-    matrix = [[0 for y in range(8)] for x in range(8)]
-    matrix[x][y] = 1
+i, j = (int(i) for i in input().split())
+# if 0 <= i <= 7 and 0 <= j <= 7:
+#     matrix = [[0 for j in range(m)] for i in range(n)]
+#     if i in range(m):
+#         for f in range(n):
+#             matrix[i][j]= 1
+#         print(matrix)
+# else:
+#     print("fuck this shit.")
+if (abs(a - i) == 2 and abs(b - j) == 1) or (abs(a - i) == 1 and abs(b - j) == 2):
+    print ("horse")
+    matrix = [[0 for j in range(m)] for i in range(n)]
+    if i in range(m):
+        for f in range(n):
+            matrix[i][j] = 1
+        print(matrix)
 else:
-    coord()
-
-#matrix = [[0 for j in range(m)] for i in range(m)]
-#matrix[i][j]=1
-print(matrix)
+    print ("not_horse")
