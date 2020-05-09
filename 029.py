00e2ab4efcbd1a8e84e22a8cb52e217383db705a
nochnoenebo <gomenasai989@gmail.com>
2020-05-09 22:56:25 +0300

    29 - 35

---
+++ 029.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Слишком много кошек! Мир обречен!")
+if people > cats:
+    print("He так много кошек! Мир спасен!")
+
+if people < dogs:
+    print("Мир утоп в слюнях!")
+
+if people > dogs:
+    print("He все так плохо!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("Людей больше или столько же, сколько собак.")
+
+if people <= dogs:
+    print("Людей меньше или столько же, сколько собак.")
+
+
+if people == dogs:
+    print("Людей столько же, сколько собак.")
